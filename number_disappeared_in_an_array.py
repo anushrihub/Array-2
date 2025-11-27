368383c790dc67ca834a7f38b1f4468418f5b153
Anushri <sastikaranushri@gmail.com>
2025-11-27 15:09:00 -0500

    optimised solution

--- number_disappeared_in_an_array.py
+++ number_disappeared_in_an_array.py
@@ -1,23 +1,27 @@
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
-# brutforce solution tc- O(n2) sc- (1)
-# such types of flags only stored temporarily inside the loop iteration. it does not presists outside the loop
+# In this problem as the range started from 1 so index is used to find the missing number. If any index is positive means that number was missing from the array. So to find the missing number we are adding 1 to the index to get the number.
+# Time Complexity- O(n) Space Complexity- O(1)
+
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        # this is an output space and it's not counted in complexity
-        result = []
         n = len(nums)
-        # this loop is giving an index so we started from 1
-        for i in range(1, n+1):
-            flag = False
-            # this loop is checking number with that index
-            for j in range(n):
-                if nums[j] == i:
-                    flag = True
-                    break
-            # default flag = false condition
-            if not flag:
-                result.append(i)
+
+        for i in range(n):
+            # index starts from 0, so we are finding the index of the number by subracting 1 because nums is starting from 1. and the absolute condition is to hangle the duplicate numbers
+            index = abs(nums[i]) - 1
+            # if the index is positive make it negative if it's already negative then don't multiply because it will make it positive, where positive stands for missing number 
+            if nums[index] > 0:
+                nums[index] *= -1
+
+        result = []
+        for i in range(n):
+            # as we marked the number to negative so missing number is positive checking that condition
+            if nums[i] > 0:
+                # if index is found then to get the number add 1 and append into the result set
+                result.append(i + 1)
         return result
+
 solution =Solution()
-print(solution.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+print(solution.findDisappearedNumbers([4,3,2,7,8,2,3,1]))      
+
